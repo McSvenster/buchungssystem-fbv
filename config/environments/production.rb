042b8df471e6834d7d454c67b4afa76867ba054a
McS <mcsvenster@gmail.com>
2017-04-30 23:37:27 +0200

    email in prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,20 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "tux83.hoststar.ch",
+    authentication: 'login',
+    user_name: "reserve@privat.koesling.it",
+    password: ENV['KIT_SMTP_PASSWORD'],
+    openssl_verify_mode: 'none'
+  }
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = {
+    host: host,
+  }
+  
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
